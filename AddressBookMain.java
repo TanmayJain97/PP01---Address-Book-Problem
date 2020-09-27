a59fcb628d02d22dbbfbc979a3c600edce9ce9ef
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-09-27 22:57:15 +0530

    Added main file

---
+++ AddressBookMain.java
@@ -0,0 +1,13 @@
+package AddressBook;
+
+import java.io.*;
+import java.util.*;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("~~~~Welcome to Address Book Program~~~~");
+		System.out.println();
+		AddressBookUC5.main(null);
+	}
+}
